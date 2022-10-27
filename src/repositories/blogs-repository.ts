e498f0f63753d0a049111f806843b1d8e8d1ccb9
Gis1990 <101275158+Gis1990@users.noreply.github.com>
2022-10-27 17:12:37 +0200

    version 0.15

--- src/repositories/blogs-repository.ts
+++ src/repositories/blogs-repository.ts
@@ -8,7 +8,7 @@ export class BlogsRepository {
         let cursor
         let totalCount
         if (SearchNameTerm) {
-            cursor = await BlogsModelClass.find({name: {$regex: SearchNameTerm}}, {_id: 0}).sort({ "createdAt": -1 }).skip(skips).sort({ "createdAt": -1 }).limit(PageSize).lean()
+            cursor = await BlogsModelClass.find({name: {$regex: SearchNameTerm}}, {_id: 0}).sort({ "createdAt": -1 }).skip(skips).limit(PageSize).lean()
             totalCount = await BlogsModelClass.count({name: {$regex: SearchNameTerm}})
         } else {
             cursor = await BlogsModelClass.find({}, {_id: 0}).sort({ "createdAt": -1 }).skip(skips).limit(PageSize).lean()
