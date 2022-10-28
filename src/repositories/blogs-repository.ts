6bfc8a047ac894fee709e3e894f503a188b7ef92
Gis1990 <101275158+Gis1990@users.noreply.github.com>
2022-10-28 17:12:28 +0200

    version 0.15

--- src/repositories/blogs-repository.ts
+++ src/repositories/blogs-repository.ts
@@ -5,23 +5,25 @@ import {BlogsModelClass} from "./db";
 export class BlogsRepository {
     async getAllBlogs(SearchNameTerm:string|null,pageNumber:number,pageSize:number,sortBy:string,sortDirection:string):Promise<BlogDBClassPagination> {
         const skips = pageSize * (pageNumber - 1)
+        let sortParameter1=`${-sortBy}`
+        let sortParameter2=`${sortBy}`
         console.log(sortBy,sortDirection)
         let cursor
         let totalCount
         if (SearchNameTerm) {
             if (sortDirection==="desc"){
-                cursor = await BlogsModelClass.find({name: {$regex: SearchNameTerm}}, {_id: 0}).sort(`${-sortBy}`).skip(skips).limit(pageSize).lean()
+                cursor = await BlogsModelClass.find({name: {$regex: SearchNameTerm}}, {_id: 0}).sort(sortParameter1).skip(skips).limit(pageSize).lean()
                 totalCount = await BlogsModelClass.count({name: {$regex: SearchNameTerm}})
             }else{
-                cursor = await BlogsModelClass.find({}, {_id: 0}).sort(`${sortBy}`).skip(skips).limit(pageSize).lean()
+                cursor = await BlogsModelClass.find({}, {_id: 0}).sort(sortParameter2).skip(skips).limit(pageSize).lean()
                 totalCount = await BlogsModelClass.count({})
             }
         }else{
             if (sortDirection==="desc"){
-                cursor = await BlogsModelClass.find({},  {_id: 0}).sort(`${-sortBy}`).skip(skips).limit(pageSize).lean()
+                cursor = await BlogsModelClass.find({},  {_id: 0}).sort(sortParameter1).skip(skips).limit(pageSize).lean()
                 totalCount = await BlogsModelClass.count({})
             }else{
-                cursor = await BlogsModelClass.find({},  {_id: 0}).sort(`${sortBy}`).skip(skips).limit(pageSize).lean()
+                cursor = await BlogsModelClass.find({},  {_id: 0}).sort(sortParameter2).skip(skips).limit(pageSize).lean()
                 totalCount = await BlogsModelClass.count({})
         }
     }
