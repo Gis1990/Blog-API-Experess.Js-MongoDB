7fc8f6c87856eecfac2273f57b8e8973266f937e
Gis1990 <101275158+Gis1990@users.noreply.github.com>
2022-12-11 18:27:05 +0100

    version 0.17.1

--- src/controllers/security-controller.ts
+++ src/controllers/security-controller.ts
@@ -22,12 +22,12 @@ export class SecurityController{
         }
     }
     async terminateSpecificDevice(req: Request, res: Response) {
-        const correct1 = await this.securityService.checkAccessRights(req.cookies.refreshToken,req.params.deviceId)
-        const correct2 = await this.securityService.checkDeviceId(req.cookies.refreshToken,req.params.deviceId)
-        if (!correct2) {
-            res.sendStatus(403)
-            return
-        }
+        // const correct1 = await this.securityService.checkAccessRights(req.cookies.refreshToken,req.params.deviceId)
+        // const correct2 = await this.securityService.checkDeviceId(req.cookies.refreshToken,req.params.deviceId)
+        // if (!correct2) {
+        //     res.sendStatus(403)
+        //     return
+        // }
         const deviceTerminated = await this.securityService.terminateSpecificDevice(req.cookies.refreshToken,req.params.deviceId)
         if (deviceTerminated) {
             res.sendStatus(204)
